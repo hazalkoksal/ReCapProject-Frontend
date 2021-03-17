ee023514afca63d580ff3271f7cef7759ab6e5fd
hazal köksal <hazal-koksal@windowslive.com>
2021-03-17 16:30:36 +0300

    ...

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { CarDetail } from 'src/app/models/carDetail';
 import { CarService } from 'src/app/services/car.service';
 
@@ -12,10 +13,20 @@ export class CarComponent implements OnInit {
   cars:CarDetail[] = [];
   dataLoaded=false;
 
-  constructor(private carService:CarService) { }
+  constructor(private carService:CarService, private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.getCars();
+    this.activatedRoute.params.subscribe(params=>{
+      if(params["brandId"]){
+        this.getCarsByBrand(params["brandId"])
+      }
+      else if(params["colorId"]){
+        this.getCarsByColor(params["colorId"])
+      }
+      else{
+        this.getCars();
+      }
+    })  
   }
 
   getCars(){
@@ -24,4 +35,19 @@ export class CarComponent implements OnInit {
       this.dataLoaded=true;
     })
   }
+  
+  getCarsByBrand(brandId:number){
+    this.carService.getCarsByBrand(brandId).subscribe(response => {
+      this.cars=response.data;
+      this.dataLoaded=true;
+    })
+  }
+
+  getCarsByColor(colorId:number){
+    this.carService.getCarsByColor(colorId).subscribe(response => {
+      this.cars=response.data;
+      this.dataLoaded=true;
+    })    
+  }
+
 }
--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -9,11 +9,22 @@ import { ListResponseModel } from '../models/listResponseModel';
 })
 export class CarService {
 
-  apiUrl = 'https://localhost:44334/api/Cars/getcardetails';
+  apiUrl = 'https://localhost:44334/api/';
 
   constructor(private httpClient:HttpClient) { }
 
   getCars():Observable<ListResponseModel<CarDetail>>{
-    return this.httpClient.get<ListResponseModel<CarDetail>>(this.apiUrl);
+    let newPath = this.apiUrl + "Cars/getcardetails"
+    return this.httpClient.get<ListResponseModel<CarDetail>>(newPath);
+  }
+
+  getCarsByBrand(brandId:number):Observable<ListResponseModel<CarDetail>>{
+    let newPath = this.apiUrl + "Cars/getdetailsbybrandid?id=" + brandId;
+    return this.httpClient.get<ListResponseModel<CarDetail>>(newPath);
+  }
+
+  getCarsByColor(colorId:number):Observable<ListResponseModel<CarDetail>>{
+    let newPath = this.apiUrl + "Cars/getdetailsbycolorid?id=" + colorId;
+    return this.httpClient.get<ListResponseModel<CarDetail>>(newPath);
   }
 }
